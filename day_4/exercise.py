a320ed9930d12134b04d779c20f09f67e7472601
crysera <240901401+Crysera@users.noreply.github.com>
2026-04-06 14:20:33 +0000

    day 4

---
+++ day_4/exercise.py
@@ -0,0 +1,21 @@
+a = "thirty"
+b = "Days"
+c = "of"
+d = "Python" 
+# Concatenate the strings a, b, c, and d to form a single string.
+result = a + " " + b + " " + c + " " + d
+print(result)   
+
+a = "Coding"
+b = "For" 
+c = "All"
+result = a + " " + b + " " + c
+print(result)
+
+company = result
+print(company)
+
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize().title().swapcase())
